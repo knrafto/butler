972f805c8f508b83f472f79efcc617d9255fcc61
Kyle Raftogianis <kylerafto@gmail.com>
2014-08-14 12:36:38 -0700

    Start all plugins

--- server/plugins/__init__.py
+++ server/plugins/__init__.py
@@ -1,2 +1,8 @@
 def start(config):
-    return {}
+    def start_plugin(plugin):
+        name = plugin.plugin_name
+        kwds = config.get(name, {})
+        return plugin(**kwds)
+    return {
+        plugin.plugin_name: start_plugin(plugin) for plugin in all_plugins
+    }

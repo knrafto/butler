1c6a96efddc60d013e264ad4e649671b33b8cb13
Kyle Raftogianis <kylerafto@gmail.com>
2014-08-17 16:22:40 -0700

    Specialize to JSON endpoints

--- server/endpoint.py
+++ server/endpoint.py
@@ -1,102 +1,24 @@
+"""JSON endpoints."""
 import functools
 import inspect
 import json
-import sys
 
-from werkzeug.exceptions import InternalServerError
 from werkzeug.routing import Map, Rule, Submount
 from werkzeug.utils import ArgumentValidationError, validate_arguments
 from werkzeug.wsgi import responder
 from werkzeug.wrappers import Request, Response
 
-def curry(f):
-    def curried(*args, **kwds):
-        return functools.partial(f, *args, **kwds)
-    return curried
-
-class Endpoint(object):
-    """An HTTP endpoint that can be used as a normal function.
-
-    :param string: The :class:Rule string.
-    :param f: The wrapped function.
-    :param response_handler: A function that, if present, will be
-        called with the result of the wrapped function.
-    :param error_handler: A function that, if present, will be
-        called with any exception the wrapped function raises.
-    :param kwds: Keyword arguments for :class:Rule.
-
-    >>> def foo(x=0):
-    ...     return x + 2
-    ...
-    >>> f = Endpoint('/<int:x>/', foo,
-    ...              response_handler=str,
-    ...              error_handler=type)
-    >>> str(f.rule)
-    '/<int:x>/'
-    >>> f(3)
-    5
-    >>> f.dispatch(3)
-    '5'
-    >>> f.dispatch('spam')
-    <type 'exceptions.TypeError'>
-    """
-
-    rule = None
-    """The :class:Rule for routing."""
-
-    def __init__(self, string, f,
-                 response_handler=None, error_handler=None,
-                 **kwds):
-        self.rule = Rule(string, endpoint=self, **kwds)
-        self.f = f
-        self.response_handler = response_handler
-        self.error_handler = error_handler
-        functools.update_wrapper(self, f)
-
-    def __call__(self, *args, **kwds):
-        """Call the wrapped function normally."""
-        return self.f(*args, **kwds)
-
-    def dispatch(self, *args, **kwds):
-        """Dispatch a request and pass URL arguments to the wrapped
-        function. Any form or query values will be passed as well.
-
-        If the function returns a value, :attribute:response_handler
-        will be called with the result. If the function raises an
-        exception, :attribute:error_handler will be called with the
-        exception object. These methods should return a WSGI
-        application, such as a :class:Response object.
-        """
-        try:
-            args, kwds = validate_arguments(self.f, args, kwds)
-        except ArgumentValidationError:
-            pass # TODO
-
-        try:
-            try:
-                result = self.f(*args, **kwds)
-            except:
-                if self.error_handler:
-                    result = self.error_handler(sys.exc_info()[1])
-            else:
-                if self.response_handler:
-                    result = self.response_handler(result)
-            return result
-        except Exception as e:
-            return InternalServerError(str(e))
-
-endpoint = curry(Endpoint)
-
-class DefaultEncoder(json.JSONEncoder):
+class Encoder(json.JSONEncoder):
     """The default JSON encoder. When encoding objects, if the object
-    has a :method:json method, it will be used to convert the object.
+    has a :attribute:json property, it will be used instead of the
+    object.
 
     >>> class Spam(object):
     ...     def json(self):
-    ...         return ['spam', 'eggs']
+    ...         return 'eggs'
     ...
-    >>> DefaultEncoder().encode({'spam': Spam()})
-    '{"spam": ["spam", "eggs"]}'
+    >>> Encoder().encode({'spam': Spam()})
+    '{"spam": "eggs"}'
     """
     def default(self, obj):
         try:
@@ -106,61 +28,120 @@ class DefaultEncoder(json.JSONEncoder):
         else:
             return encode()
 
-class JSONEndpoint(Endpoint):
+class JSONRequest(Request):
+    """A JSON request.
+
+    >>> from werkzeug.test import EnvironBuilder
+    >>> environ = EnvironBuilder(
+    ...     content_type='application/json',
+    ...     data='{"spam": 1, "eggs": 2}').get_environ()
+    >>> d = JSONRequest(environ).json()
+    >>> d['spam']
+    1
+    >>> d['eggs']
+    2
+    """
+    max_content_length = 1024 * 1024 # 1MB
+
+    def json(self):
+        if self.headers.get('content-type') == 'application/json':
+            return json.loads(self.get_data(as_text=True))
+
+class JSONResponse(Response):
+    """A JSON response.
+
+    >>> class Spam(object):
+    ...     def json(self):
+    ...         return 'eggs'
+    ...
+    >>> response = JSONResponse({"spam": Spam()}, status=200)
+    >>> response.content_type
+    'application/json'
+    >>> response.get_data()
+    b'{"spam": "eggs"}'
+    """
+    def __init__(self, result, encoder=None, **kwds):
+        super(JSONResponse, self).__init__(**kwds)
+        if encoder is None:
+            encoder = Encoder()
+        self.response = encoder.iterencode(result)
+        self.content_type = 'application/json'
+
+class Endpoint:
     """An JSON endpoint that can be used as a normal function.
 
     :param string: The :class:Rule string.
     :param f: The wrapped function.
-    :param encoder: A :class:JSONEncoder. The default encoder is
-        :class:DefaultEncoder.
-    :param exceptions: A list of (type, status) pairs that will
-        be used to encode failures. The exception types are tried in
-        order to determine the status of an exception. The default
-        status code is 500: Internal Server Error.
+    :param response_handler: A function that, if present, will be
+        called with the result of the wrapped function.
+    :param error_handler: A function that, if present, will be
+        called with any exception the wrapped function raises.
+    :param kwds: Keyword arguments for :class:Rule.
 
-    >>> def foo(x=0):
-    ...     if not isinstance(x, int):
-    ...         raise TypeError('oops')
-    ...     return x + 2
+    >>> from werkzeug.test import Client
+    >>> def foo(x=0, y=0):
+    ...     return {'result': x + y}
     ...
-    >>> f = JSONEndpoint('/<int:x>/', foo,
-    ...                  encoder=DefaultEncoder(sort_keys=True),
-    ...                  exceptions=[(TypeError, 400)])
+    >>> f = Endpoint('/<int:x>/', foo, encoder=Encoder(sort_keys=True))
     >>> str(f.rule)
     '/<int:x>/'
-    >>> f(3)
-    5
-    >>> f.dispatch(3).data
-    '5'
-    >>> f.dispatch('spam').data
-    '{"message": "oops", "status": 400}'
+    >>> f(3, 2)
+    {'result': 5}
+    >>> c = Client(f.dispatch({"x": 3}), Response)
+    >>> c.get('/', data='{"y": 2}',
+    ...       content_type='application/json').data
+    b'{"result": 5}'
+    >>> c.get('/', data='{"y": "spam"}',
+    ...       content_type='application/json').data # doctest: +ELLIPSIS
+    b'{"message": "...", "status": 500}'
     """
-    def __init__(self, string, f, encoder=None, exceptions=None, **kwds):
-        super(JSONEndpoint, self).__init__(
-            string, f,
-            response_handler=self._encode_response,
-            error_handler=self._encode_error,
-            **kwds)
-        self.encoder = encoder or DefaultEncoder()
-        self.exceptions = exceptions or []
+    def __init__(self, string, f, encoder=None, **kwds):
+        self.rule = Rule(string, endpoint=self, **kwds)
+        self.f = f
+        self.encoder = encoder
+        functools.update_wrapper(self, f)
 
-    def _encode_response(self, result, status=None):
-        return Response(
-            self.encoder.iterencode(result), status,
-            content_type='application/json')
+    def __call__(self, *args, **kwds):
+        """Call the wrapped function normally."""
+        return self.f(*args, **kwds)
 
-    def _encode_error(self, e):
-        status = 500
-        for exctypes, code in self.exceptions:
-            if isinstance(e, exctypes):
-                status = code
-                break
-        return self._encode_response({
-            'status': status,
-            'message': str(e)
-        }, status)
+    def dispatch(self, kwds):
+        """Dispatch a request and pass URL arguments to the wrapped
+        function. Any JSON object in the request body is passed as
+        well if parsed correctly. Returns a WSGI application.
+        """
+        @responder
+        def app(environ, start_reponse):
+            status = None
+            try:
+                result = self._marshal(JSONRequest(environ), kwds)
+            except Exception as e:
+                try:
+                    status = e.status
+                except AttributeError:
+                    status = 500 # Internal server error
+                result = {
+                    'status': status,
+                    'message': str(e)
+                }
+            return JSONResponse(result, encoder=self.encoder, status=status)
+        return app
 
-json_endpoint = curry(JSONEndpoint)
+    def _marshal(self, request, kwds):
+        try:
+            body = request.json()
+            if isinstance(body, dict):
+                kwds.update(body)
+        except ValueError:
+            pass
+        try:
+            args, kwds = validate_arguments(self.f, (), kwds)
+        except ArgumentValidationError as e:
+            raise BadRequest(e)
+        return self.f(*args, **kwds)
+
+def endpoint(string, **kwds):
+    return functools.partial(Endpoint, string, **kwds)
 
 class Dispatcher(object):
     """A WSGI application that dispatches requests to delegates.
@@ -174,19 +155,17 @@ class Dispatcher(object):
 
     :param delegates: A dictionary of delegates.
 
-    >>> from werkzeug.exceptions import InternalServerError
     >>> from werkzeug.test import Client
     >>> from werkzeug.wrappers import Response
     >>> class Spam(object):
-    ...     @endpoint('/<int:x>/', response_handler=Response)
+    ...     @endpoint('/<int:x>/', encoder=Encoder(sort_keys=True))
     ...     def foo(x=0):
-    ...         return str(x + 2)
+    ...         return {'result': x + 2}
     ...
     >>> class Eggs(object):
-    ...     @endpoint('/<string:s>/',
-    ...               error_handler=InternalServerError)
+    ...     @endpoint('/<string:s>/', encoder=Encoder(sort_keys=True))
     ...     def foo(s=''):
-    ...         raise Exception('ni')
+    ...         raise BadRequest(s)
     ...
     >>> dispatcher = Dispatcher({
     ...     'spam': Spam(),
@@ -194,9 +173,9 @@ class Dispatcher(object):
     ... })
     >>> c = Client(dispatcher, Response)
     >>> c.get('/spam/3/').data
-    '5'
-    >>> c.get('/eggs/ni/').data[:14]
-    '<!DOCTYPE HTML'
+    b'{"result": 5}'
+    >>> c.get('/eggs/ni/').data
+    b'{"message": "ni", "status": 400}'
     """
     def __init__(self, delegates):
         self.delegates = delegates
@@ -206,17 +185,17 @@ class Dispatcher(object):
                     delegate, lambda f: isinstance(f, Endpoint)
                 )
             ])
-            for name, delegate in self.delegates.iteritems()
+            for name, delegate in self.delegates.items()
         ])
 
     @responder
     def __call__(self, environ, start_reponse):
         """Respond to a request."""
-        request = Request(environ)
         urls = self.url_map.bind_to_environ(environ)
+        return urls.dispatch(lambda f, kwds: f.dispatch(kwds))
 
-        def dispatch(f, kwds):
-            kwds.update(request.values.to_dict())
-            return f.dispatch(**kwds)
+class HTTPException(Exception):
+    status = 400
 
-        return urls.dispatch(dispatch)
+class BadRequest(HTTPException):
+    status = 400

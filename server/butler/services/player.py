2d7acd835e12d9f0c1efacb74bac74380ac3a292
Kyle Raftogianis <kylerafto@gmail.com>
2014-08-26 15:03:12 -0700

    Player service

---
+++ server/butler/services/player.py
@@ -0,0 +1,178 @@
+import collections
+import random
+
+from butler.options import Options
+from butler.routing import endpoint
+from butler.service import singleton
+from butler.utils import Counter, Queue
+
+class Metadata(collections.namedtuple(
+        'Metadata',
+        'name artist duration url artwork_url source_image_url')):
+    def json(self):
+        return self._asdict()
+
+class Track(object):
+    def __init__(self, metadata):
+        self.metadata = metadata
+
+    def load(self):
+        raise NotImplementedError
+
+    def unload(self):
+        raise NotImplementedError
+
+    def prefetch(self):
+        raise NotImplementedError
+
+    def play(self, play=True):
+        raise NotImplementedError
+
+    def seek(self, seconds):
+        raise NotImplementedError
+
+    def json(self):
+        return {
+            'type': 'track',
+            'metadata': self.metadata
+        }
+
+class TrackSet(object):
+    def __init__(self, metadata, tracks, shuffle=False):
+        if not tracks:
+            raise ValueError('empty track set')
+        self.metadata = metadata
+        self.tracks = tracks
+        if shuffle:
+            random.shuffle(self.tracks)
+
+    def __iter__(self):
+        return self
+
+    def next(self):
+        try:
+            return self.tracks.pop(0)
+        except IndexError:
+            raise StopIteration
+
+    def json(self):
+        return {
+            'type': 'set',
+            'metadata': self.metadata,
+            'tracks': self.tracks
+        }
+
+    @classmethod
+    def singleton(cls, track):
+        return cls(track.metadata, [track])
+
+@singleton
+class Player(object):
+    """A music player service."""
+    name = 'player'
+    depends = ['options']
+
+    def __init__(self, options):
+        options = options.options(self.name)
+        history_size = options.int('history_size', None)
+        self.playing = False
+        self.current_track = None
+        self.history = Queue(size=history_size)
+        self.queue = []
+        self.state_counter = Counter()
+
+    def _sync_player(self):
+        """Load and play the current track, and prefetch the next."""
+        lineup = [track for track_set in self.queue
+            for track in track_set.tracks]
+        try:
+            track = lineup[0]
+        except IndexError:
+            track = None
+        if track != self.current_track:
+            if self.current_track:
+                self.current_track.play(play=False)
+                self.current_track.unload()
+            if track:
+                track.load()
+                if self.playing or not self.current_track:
+                    track.play()
+                    self.playing = True
+            else:
+                self.playing = False
+            self.current_track = track
+        try:
+            next_track = lineup[1]
+        except IndexError:
+            pass
+        else:
+            next_track.prefetch()
+        self.state_counter.set()
+
+    @endpoint('/state/')
+    def state(self, **kwds):
+        """Return the current player state."""
+        counter = Options(kwds).int('counter', None)
+        counter = self.state_counter.wait(counter)
+        d = {prop: getattr(self, prop)
+            for prop in 'playing current_track queue history'.split()}
+        d['counter'] = counter
+        return d
+
+    @endpoint('/next_track/', methods=['POST'])
+    def next_track(self, **kwds):
+        """Load and play the next track."""
+        try:
+            track_set = self.queue[0]
+        except IndexError:
+            pass
+        else:
+            last_track = track_set.next()
+            if not track_set.tracks:
+                self.queue.pop(0)
+            self.history.insert(0, last_track)
+        self._sync_player()
+
+    @endpoint('/prev_track/', methods=['POST'])
+    def prev_track(self, **kwds):
+        """Load and play the previous track."""
+        try:
+            prev_track = self.history.pop(0)
+        except IndexError:
+            pass
+        else:
+            self.queue.insert(0, TrackSet.singleton(prev_track))
+        self._sync_player()
+
+    @endpoint('/next_set/', methods=['POST'])
+    def next_set(self, **kwds):
+        """Load and play the next track set."""
+        if self.current_track:
+            self.history.insert(0, self.current_track)
+        try:
+            self.queue.pop(0)
+        except IndexError:
+            pass
+        self._sync_player()
+
+    @endpoint('/play/', methods=['POST'])
+    def play(self, **kwds):
+        """Resume spotify playback.
+
+        Parameters:
+            play: If false, pause playback
+            seek: Seek position, in milliseconds
+        """
+        options = Options(kwds)
+        seek = options.float('seek', None)
+        play = options.bool('play')
+        if self.current_track:
+            if seek is not None:
+                self.current_track.seek(seek)
+            self.playing = play
+            self.current_track.play(play=play)
+
+    def add(self, index, track_set):
+        """Add a track at an index in the queue."""
+        self.queue.insert(index, track_set)
+        self._sync_player()
---
+++ server/butler/tests/test_player.py
@@ -0,0 +1,207 @@
+import gevent
+import unittest
+import mock
+
+from butler.options import Options
+from butler.services.player import Metadata, Player, Track, TrackSet
+
+class PlayerTestCase(unittest.TestCase):
+    data = {
+        'name': 'spam',
+        'artist': 'eggs',
+        'duration': 1.0,
+        'url': 'http://www.foo.org/123',
+        'artwork_url': 'http://www.foo.org/123/artwork.jpg',
+        'source_image_url': 'http://www.foo.org/logo.jpg'
+    }
+
+    def _mock_tracks(self, count=10, cls=Track):
+        for i in range(count):
+            data = self.data.copy()
+            data['name'] = 'track%i' % (i + 1)
+            yield mock.Mock(spec=Track)(Metadata(**data))
+
+    def test_json(self):
+        metadata = Metadata(**self.data)
+        self.assertEqual(metadata.json(), self.data)
+
+        track = Track(Metadata(**self.data))
+        self.assertEqual(track.json(), {
+            'type': 'track',
+            'metadata': metadata,
+        })
+
+        tracks = list(self._mock_tracks())
+        track_set = TrackSet(metadata, tracks)
+        self.assertEqual(track_set.json(), {
+            'type': 'set',
+            'metadata': metadata,
+            'tracks': tracks
+        })
+
+    def test_empty_set(self):
+        metadata = Metadata(**self.data)
+        with self.assertRaises(ValueError):
+            TrackSet(metadata, [])
+
+    def test_options(self):
+        service = Player(Options({
+            'player': {'history_size': 3}
+        }))
+        self.assertEqual(service.history.size, 3)
+
+    def test_state(self):
+        service = Player(Options())
+        track1, track2, track3, track4, track5, track6 = \
+            self._mock_tracks(6)
+        service.history.extend([track3, track2, track1])
+        service.queue.extend([
+            TrackSet(None, [track4, track5]),
+            TrackSet(None, [track6])
+        ])
+
+        counter = service.state()['counter']
+        marker = []
+
+        def wait_counter():
+            service.state(counter=counter)
+            marker.append(1)
+
+        gevent.spawn(wait_counter)
+        gevent.sleep()
+        self.assertFalse(marker)
+
+        service.next_track()
+        gevent.sleep()
+        self.assertTrue(marker)
+
+    def test_next_track(self):
+        service = Player(Options({
+            'player': {'history_size': 3}
+        }))
+
+        track1, track2, track3, track4, track5, track6 = \
+            self._mock_tracks(6)
+        service.history.extend([track3, track2, track1])
+        service.queue.extend([
+            TrackSet(None, [track4, track5]),
+            TrackSet(None, [track6])
+        ])
+
+        service.next_track()
+        track5.load.assert_called_with()
+        track5.play.assert_called_with()
+        track6.prefetch.assert_called_with()
+        self.assertTrue(service.playing)
+        self.assertEqual(service.history, [track4, track3, track2])
+        self.assertEqual(service.current_track, track5)
+
+        service.next_track()
+        track5.play.assert_called_with(play=False)
+        track5.unload.assert_called_with()
+        track6.load.assert_called_with()
+        track6.play.assert_called_with()
+        self.assertTrue(service.playing)
+        self.assertEqual(service.history, [track5, track4, track3])
+        self.assertEqual(service.current_track, track6)
+        self.assertEqual(len(service.queue), 1)
+
+        service.next_track()
+        track6.play.assert_called_with(play=False)
+        track6.unload.assert_called_with()
+        self.assertFalse(service.playing)
+        self.assertEqual(service.current_track, None)
+        self.assertEqual(len(service.queue), 0)
+
+    def test_prev_track(self):
+        service = Player(Options({
+            'player': {'history_size': 3}
+        }))
+
+        track1, track2, track3, track4, track5, track6 = \
+            self._mock_tracks(6)
+        service.history.extend([track3, track2, track1])
+        service.queue.extend([
+            TrackSet(None, [track4, track5]),
+            TrackSet(None, [track6])
+        ])
+
+        service.prev_track()
+        track3.load.assert_called_with()
+        track3.play.assert_called_with()
+        self.assertTrue(service.playing)
+        self.assertEqual(service.history, [track2, track1])
+        self.assertEqual(service.current_track, track3)
+
+        service.next_track()
+        self.assertTrue(service.playing)
+        self.assertEqual(service.history, [track3, track2, track1])
+        self.assertEqual(service.current_track, track4)
+
+        for _ in range(5):
+            service.prev_track()
+        self.assertTrue(service.playing)
+        self.assertEqual(service.current_track, track1)
+        self.assertEqual(len(service.queue), 5)
+
+    def test_next_set(self):
+        service = Player(Options({
+            'player': {'history_size': 3}
+        }))
+
+        track1, track2, track3, track4, track5, track6 = \
+            self._mock_tracks(6)
+        service.history.extend([track2, track1])
+        service.queue.extend([
+            TrackSet(None, [track3, track4, track5]),
+            TrackSet(None, [track6])
+        ])
+
+        service.next_track()
+        service.next_set()
+        self.assertTrue(service.playing)
+        self.assertEqual(service.current_track, track6)
+        self.assertEqual(service.history, [track4, track3, track2])
+
+    def test_play(self):
+        service = Player(Options({
+            'player': {'history_size': 3}
+        }))
+
+        track1, track2, track3, track4, track5, track6 = \
+            self._mock_tracks(6)
+        service.history.extend([track3, track2, track1])
+        service.queue.extend([
+            TrackSet(None, [track4, track5]),
+            TrackSet(None, [track6])
+        ])
+
+        service.next_track()
+        track5.load.assert_called_with()
+        track5.play.assert_called_with()
+        track6.prefetch.assert_called_with()
+        self.assertTrue(service.playing)
+
+        service.play(play=False)
+        track5.play.assert_called_with(play=False)
+        self.assertFalse(service.playing)
+
+        service.play(play=False, seek=3.14)
+        track5.seek.assert_called_with(3.14)
+
+        service.next_track()
+        self.assertEqual(service.current_track, track6)
+        self.assertFalse(service.playing)
+        self.assertFalse(track6.play.called)
+
+    def test_add(self):
+        service = Player(Options({
+            'player': {'history_size': 3}
+        }))
+
+        track1, track2, track3, track4, track5, track6 = \
+            self._mock_tracks(6)
+
+        service.add(0, TrackSet(None, [track1, track2, track3]))
+        self.assertTrue(service.playing)
+        self.assertEqual(service.current_track, track1)

f94bd77d93fc3c8e73fee8bf684c5301b550e0e2
Kyle Raftogianis <kylerafto@gmail.com>
2014-09-01 13:02:24 -0700

    Set up testing

--- mobile/tests/karma.conf.js
+++ mobile/tests/karma.conf.js
@@ -4,7 +4,13 @@ module.exports = function(config){
     basePath : '../',
 
     files : [
-      'www/lib/ionic/js/ionic.bundle.js'
+      'www/lib/ionic/js/angular/angular.js',
+      'www/lib/ionic/js/angular/angular-animate.js',
+      'www/lib/ionic/js/angular/angular-mocks.js',
+      'www/lib/ionic/js/angular/angular-sanitize.min.js',
+      'www/lib/ionic/js/angular-ui/angular-ui-router.js',
+      'www/lib/ionic/js/ionic.js',
+      'www/lib/ionic/js/ionic-angular.min.js',
       'www/js/*.js',
       'tests/unit/*.js'
     ],
@@ -17,7 +23,6 @@ module.exports = function(config){
 
     plugins : [
       'karma-chrome-launcher',
-      'karma-firefox-launcher',
       'karma-jasmine'
     ],
 

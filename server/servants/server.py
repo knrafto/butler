db4e6eebf579fce97d639185976b672ea4176bb5
Kyle Raftogianis <kylerafto@gmail.com>
2014-09-11 01:44:55 -0700

    Create server plugin

---
+++ server/servants/server.py
@@ -0,0 +1,66 @@
+import functools
+
+import gevent
+import socketio
+import socketio.namespace
+import socketio.server
+
+import butler
+
+class Server(butler.Servant):
+    """A web server that communicates through Socket.IO."""
+    name = 'server'
+
+    def __init__(self, butler, config):
+        super(Server, self).__init__(butler, config)
+        address = config.get('address', '127.0.0.1:26532')
+        server = socketio.server.SocketIOServer(
+            address, self._respond, resource="socket.io")
+        gevent.spawn(server.serve_forever())
+
+    def _respond(self, environ, start_response):
+        path = environ['PATH_INFO'].strip('/')
+        if path.startswith("socket.io"):
+            socketio.socketio_manage(environ, {'': Namespace}, self.butler)
+        else:
+            start_response('404 Not Found', [])
+            return ()
+
+class Namespace(socketio.namespace.BaseNamespace):
+    def initialize(self):
+        self.butler = self.request
+        self.listeners = []
+
+    def disconnect(self):
+        for event, listener in self.listeners:
+            self.butler.off(event, listener)
+
+    def process_event(self, packet):
+        try:
+            name = packet['name']
+            data = packet['args'][0]
+            if name == 'event':
+                self.butler.emit(data['name'], *data['args'], **data['kwds'])
+            elif name == 'request':
+                result = self.butler.call(
+                    data['method'], *data['args'], **data['kwds'])
+                self.emit('response', {
+                    'id': data['id'],
+                    'result': result
+                })
+            elif name == 'subscribe':
+                event = data['name']
+                listener = functools.partial(self.notify, event)
+                self.listeners.append((event, listener))
+                self.butler.on(event, listener)
+            else:
+                raise ValueError("unknown packet name '%s'" % name)
+        except Exception as e:
+            self.error(e.__class__.__name__, str(e))
+
+    def notify(self, event, *args, **kwds):
+        self.emit('event', {
+            'name': event,
+            'args': args,
+            'kwds': kwds
+        })
---
+++ server/tests/test_server.py
@@ -0,0 +1,85 @@
+import mock
+import unittest
+
+import socketio.virtsocket
+
+import butler
+from servants import server
+
+class ServerTestCase(unittest.TestCase):
+    def setUp(self):
+        self.butler = mock.Mock(spec=butler.Butler)
+        self.socket = mock.Mock()
+        environ = {'socketio': self.socket}
+        self.ns = server.Namespace(environ, '', request=self.butler)
+        self.ns.initialize()
+
+    def test_event(self):
+        self.ns.process_event({
+            'type': 'event',
+            'name': 'event',
+            'args': [{
+                'name': 'foo.bar',
+                'args': ['spam', 'eggs'],
+                'kwds': {'knights': 'ni'}
+            }]
+        })
+        self.assertFalse(self.socket.error.called)
+        self.butler.emit.assert_called_with(
+            'foo.bar', 'spam', 'eggs', knights='ni')
+
+    def test_request(self):
+        self.butler.call.return_value = 42
+        self.ns.process_event({
+            'type': 'event',
+            'name': 'request',
+            'args': [{
+                'method': 'foo.bar',
+                'id': 5,
+                'args': ['spam', 'eggs'],
+                'kwds': {'knights': 'ni'}
+            }]
+        })
+        self.assertFalse(self.socket.error.called)
+        self.butler.call.assert_called_with(
+            'foo.bar', 'spam', 'eggs', knights='ni')
+        self.socket.send_packet.assert_called_with({
+            'type': 'event',
+            'name': 'response',
+            'endpoint': '',
+            'args': ({
+                'id': 5,
+                'result': 42
+            },)
+        })
+
+    def test_subscribe(self):
+        callbacks = []
+        self.butler.on.side_effect = lambda event, f: callbacks.append(f)
+        self.ns.process_event({
+            'type': 'event',
+            'name': 'subscribe',
+            'args': [{
+                'name': 'foo.bar'
+            }]
+        })
+        self.assertFalse(self.socket.error.called)
+        self.assertEqual(len(callbacks), 1)
+        callbacks[0]('spam', 'eggs', knights='ni')
+
+    def test_error(self):
+        self.butler.call.side_effect = TypeError('bam')
+        self.ns.process_event({
+            'type': 'event',
+            'name': 'request',
+            'args': [{
+                'method': 'foo.bar',
+                'id': 5,
+                'args': ['spam', 'eggs'],
+                'kwds': {'knights': 'ni'}
+            }]
+        })
+        self.socket.error.assert_called_with(
+            'TypeError', 'bam', msg_id=None, endpoint='', quiet=False)
+        self.butler.call.assert_called_with(
+            'foo.bar', 'spam', 'eggs', knights='ni')

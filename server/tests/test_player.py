ea14f8b3321bebdba7a856ea5d2c9c33b7cbc400
Kyle Raftogianis <kylerafto@gmail.com>
2014-09-12 14:19:41 -0700

    Tweak player tests

--- server/tests/test_player.py
+++ server/tests/test_player.py
@@ -7,6 +7,7 @@ from servants import player
 class PlayerTestCase(unittest.TestCase):
     def setUp(self):
         self.butler = mock.Mock(spec=butler.Butler)
+        self.player = player.Player(self.butler, {'history_size': 3})
 
     def _mock_track(self):
         return mock.Mock(spec=player.Track)
@@ -25,14 +26,12 @@ class PlayerTestCase(unittest.TestCase):
         self.assertEqual(track.json(), data)
 
     def test_next_track(self):
-        servant = player.Player(self.butler, {'history_size': 3})
-
         track1, track2, track3, track4, track5, track6 = (
             self._mock_track() for _ in range(6))
-        servant.history.extend([track3, track2, track1])
-        servant.queue.extend([track4, track5, track6])
+        self.player.history.extend([track3, track2, track1])
+        self.player.queue.extend([track4, track5, track6])
 
-        servant.next_track()
+        self.player.next_track()
         track5.load.assert_called_with()
         track5.play.assert_called_with()
         track6.prefetch.assert_called_with()
@@ -43,8 +42,8 @@ class PlayerTestCase(unittest.TestCase):
             current_track=track5,
             queue=[track5, track6])
 
-        servant.seek(314)
-        servant.next_track()
+        self.player.seek(314)
+        self.player.next_track()
         track5.play.assert_called_with(play=False)
         track5.unload.assert_called_with()
         track6.load.assert_called_with()
@@ -56,8 +55,8 @@ class PlayerTestCase(unittest.TestCase):
             current_track=track6,
             queue=[track6])
 
-        servant.seek(314)
-        servant.next_track()
+        self.player.seek(314)
+        self.player.next_track()
         track6.play.assert_called_with(play=False)
         track6.unload.assert_called_with()
         self.butler.emit.assert_called_with(
@@ -68,15 +67,13 @@ class PlayerTestCase(unittest.TestCase):
             queue=[])
 
     def test_prev_track(self):
-        servant = player.Player(self.butler, {})
-
         track1, track2, track3, track4, track5, track6 = (
             self._mock_track() for _ in range(6))
-        servant.history.extend([track3, track2, track1])
-        servant.queue.extend([track4, track5, track6])
+        self.player.history.extend([track3, track2, track1])
+        self.player.queue.extend([track4, track5, track6])
 
-        servant.seek(314)
-        servant.prev_track()
+        self.player.seek(314)
+        self.player.prev_track()
         track3.load.assert_called_with()
         track3.play.assert_called_with()
         self.butler.emit.assert_called_with(
@@ -86,7 +83,7 @@ class PlayerTestCase(unittest.TestCase):
             current_track=track3,
             queue=[track3, track4, track5, track6])
 
-        servant.next_track()
+        self.player.next_track()
         self.butler.emit.assert_called_with(
             'player.state',
             playing=True,
@@ -94,9 +91,9 @@ class PlayerTestCase(unittest.TestCase):
             current_track=track4,
             queue=[track4, track5, track6])
 
-        servant.seek(314)
+        self.player.seek(314)
         for _ in range(5):
-            servant.prev_track()
+            self.player.prev_track()
         self.butler.emit.assert_called_with(
             'player.state',
             playing=True,
@@ -105,77 +102,72 @@ class PlayerTestCase(unittest.TestCase):
             queue=[track1, track2, track3, track4, track5, track6])
 
     def test_play(self):
-        servant = player.Player(self.butler, {})
-
         track1, track2, track3, track4, track5, track6 = (
             self._mock_track() for _ in range(6))
-        servant.history.extend([track3, track2, track1])
-        servant.queue.extend([track4, track5, track6])
+        self.player.history.extend([track3, track2, track1])
+        self.player.queue.extend([track4, track5, track6])
 
-        servant.next_track()
+        self.player.next_track()
         track5.load.assert_called_with()
         track5.play.assert_called_with()
         track6.prefetch.assert_called_with()
         self.butler.emit.assert_called_with(
             'player.state',
             playing=True,
-            history=[track4, track3, track2, track1],
+            history=[track4, track3, track2],
             current_track=track5,
             queue=[track5, track6])
 
-        servant.play(False)
+        self.player.play(False)
         track5.play.assert_called_with(play=False)
         self.butler.emit.assert_called_with(
             'player.state',
             playing=False,
-            history=[track4, track3, track2, track1],
+            history=[track4, track3, track2],
             current_track=track5,
             queue=[track5, track6])
 
-        servant.play(True)
+        self.player.play(True)
         track5.play.assert_called_with(play=True)
         self.butler.emit.assert_called_with(
             'player.state',
             playing=True,
-            history=[track4, track3, track2, track1],
+            history=[track4, track3, track2],
             current_track=track5,
             queue=[track5, track6])
 
-        servant.play(False)
-        servant.next_track()
+        self.player.play(False)
+        self.player.next_track()
         self.butler.emit.assert_called_with(
             'player.state',
             playing=False,
-            history=[track5, track4, track3, track2, track1],
+            history=[track5, track4, track3],
             current_track=track6,
             queue=[track6])
         self.assertFalse(track6.play.called)
 
     def test_seek(self):
-        servant = player.Player(self.butler, {'history_size': 3})
-
         track1, track2, track3, track4, track5, track6 = (
             self._mock_track() for _ in range(6))
-        servant.history.extend([track3, track2, track1])
-        servant.queue.extend([track4, track5, track6])
+        self.player.history.extend([track3, track2, track1])
+        self.player.queue.extend([track4, track5, track6])
 
-        servant.next_track()
-        servant.seek(314)
+        self.player.next_track()
+        self.player.seek(314)
         track5.seek.assert_called_with(314)
         self.butler.emit.assert_called_with('player.seek', 314)
 
     @mock.patch.object(player, 'random', autospec=True)
     def test_add(self, random):
-        servant = player.Player(self.butler, {'history_size': 3})
-
         track1, track2, track3, track4, track5, track6 = (
             self._mock_track() for _ in range(6))
 
-        servant.add(0, [track1, track2, track3])
-        servant.add(2, [track4, track5, track6])
-        self.assertTrue(servant.playing)
-        self.assertEqual(servant.current_track, track1)
-        self.assertEqual(servant.queue, [track1, track2, track4, track5, track6, track3])
+        self.player.add(0, [track1, track2, track3])
+        self.player.add(2, [track4, track5, track6])
+        self.assertTrue(self.player.playing)
+        self.assertEqual(self.player.current_track, track1)
+        self.assertEqual(self.player.queue, [track1, track2, track4, track5, track6, track3])
+        self.assertFalse(random.shuffle.called)
 
-        servant.add(3, [track2, track3, track4], shuffle=True)
+        self.player.add(3, [track2, track3, track4], shuffle=True)
         random.shuffle.assert_called_with([track2, track3, track4])

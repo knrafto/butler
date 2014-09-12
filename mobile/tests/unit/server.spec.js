1b88139e1f90c86120ae3b245a4f86fdd43cb120
Kyle Raftogianis <kylerafto@gmail.com>
2014-09-11 17:21:00 -0700

    Create client Socket.IO service

---
+++ mobile/tests/unit/server.spec.js
@@ -0,0 +1,168 @@
+describe('service: server', function() {
+  var server_url = 'http://www.example.com',
+      $rootScope,
+      server,
+      socket,
+      callbacks;
+
+  function emit(name) {
+    var args = Array.prototype.slice.call(arguments, 1);
+    angular.forEach(callbacks[name] || [], function(f) {
+      f.apply(null, args);
+    });
+  }
+
+  beforeEach(module('server'));
+
+  beforeEach(inject(function($window, _$rootScope_, _server_) {
+    callbacks = {}
+    socket = {
+      emit: function() {},
+
+      on: function(name, f) {
+        (callbacks[name] = callbacks[name] || []).push(f);
+      }
+    };
+
+    spyOn(socket, 'emit');
+
+    $window.io = function(url) {
+      socket.url = url;
+      return socket;
+    };
+
+    $rootScope = _$rootScope_;
+    server = _server_(server_url);
+  }));
+
+  it('should create socket to server', function() {
+    expect(socket.url).toEqual(server_url);
+  })
+
+  it('should emit events on a socket', function() {
+    server.emit('foo.bar', [1, 2], {x: 3});
+    expect(socket.emit).toHaveBeenCalledWith('event', {
+      name: 'foo.bar',
+      args: [1, 2],
+      kwds: {x: 3}
+    });
+
+    server.emit('foo.bar');
+    expect(socket.emit).toHaveBeenCalledWith('event', {
+      name: 'foo.bar',
+      args: [],
+      kwds: {}
+    });
+  });
+
+  it('should send requests', function() {
+    server.request('foo.bar', [1, 2], {x: 3});
+    expect(socket.emit).toHaveBeenCalledWith('request', {
+      id: 0,
+      method: 'foo.bar',
+      args: [1, 2],
+      kwds: {x: 3}
+    });
+
+    server.request('foo.bar');
+    expect(socket.emit).toHaveBeenCalledWith('request', {
+      id: 1,
+      method: 'foo.bar',
+      args: [],
+      kwds: {}
+    });
+  });
+
+  it('should respond to requests', function() {
+    container = {
+      success0: function() {},
+      success1: function() {},
+      success2: function() {},
+      failure: function() {}
+    };
+
+    spyOn(container, 'success0');
+    spyOn(container, 'success1');
+    spyOn(container, 'success2');
+    spyOn(container, 'failure');
+
+    server.request('foo.bar').then(container.success0, container.failure);
+    server.request('foo.baz').then(container.success1, container.failure);
+    server.request('foo.quux').then(container.success2, container.failure);
+
+    emit('response', {
+      id: 10,
+      result: 'result'
+    });
+    expect(container.success0).not.toHaveBeenCalled();
+    expect(container.success1).not.toHaveBeenCalled();
+    expect(container.success2).not.toHaveBeenCalled();
+
+    emit('response', {
+      id: 1,
+      result: 'result'
+    });
+    $rootScope.$apply();
+    expect(container.success1).toHaveBeenCalledWith('result');
+
+    emit('response', {
+      id: 2,
+      result: 'result'
+    });
+    $rootScope.$apply();
+    expect(container.success2).toHaveBeenCalledWith('result');
+
+    emit('response', {
+      id: 0,
+      result: 'result'
+    });
+    $rootScope.$apply();
+    expect(container.success0).toHaveBeenCalledWith('result');
+
+    expect(container.failure).not.toHaveBeenCalled();
+  });
+
+  it('should reject requests on error', function() {
+    container = {
+      success: function() {},
+      failure0: function() {},
+      failure1: function() {}
+    };
+
+    spyOn(container, 'success');
+    spyOn(container, 'failure0');
+    spyOn(container, 'failure1');
+
+    server.request('foo.bar').then(container.success, container.failure0);
+    server.request('foo.baz').then(container.success, container.failure1);
+
+    emit('error', 'SomeError', 'bam');
+    $rootScope.$apply();
+    expect(container.success).not.toHaveBeenCalled();
+    expect(container.failure0).toHaveBeenCalledWith('SomeError: bam');
+    expect(container.failure1).toHaveBeenCalledWith('SomeError: bam');
+  });
+
+  it('should subscribe to the server', function() {
+    server.on('foo.bar', function() {});
+    expect(socket.emit).toHaveBeenCalledWith('subscribe', {
+      name: 'foo.bar'
+    });
+  });
+
+  it('should call callbacks on an event', function() {
+    container = {
+      f: function() {}
+    };
+
+    spyOn(container, 'f');
+
+    server.on('foo.bar', container.f);
+    emit('event', {
+      name: 'foo.bar',
+      args: [1, 2],
+      kwds: {x: 3}
+    });
+    expect(container.f).toHaveBeenCalledWith([1, 2], {x: 3});
+  });
+});
--- mobile/www/js/server.js
+++ mobile/www/js/server.js
@@ -1,1 +1,64 @@
-angular.module('server', []).constant('SERVER_URL', 'http://127.0.0.1:26532');
+angular.module('server', [])
+
+.factory('server', function($window, $q) {
+  return function(url) {
+    var socket = $window.io(url),
+        nextId = 0,
+        pendingRequests = {},
+        callbacks = {};
+
+    socket.on('response', function(data) {
+      var deferred = pendingRequests[data.id];
+      if (deferred) {
+        deferred.resolve(data.result);
+        delete pendingRequests[data.id];
+      }
+    });
+
+    socket.on('error', function(error_name, error_message) {
+      angular.forEach(pendingRequests, function(deferred) {
+        deferred.reject(error_name + ': ' + error_message);
+      });
+      pendingRequests = {};
+    });
+
+    socket.on('event', function(data) {
+      angular.forEach(callbacks[data.name] || [], function(f) {
+        f(data.args, data.kwds);
+      });
+    });
+
+    return {
+      emit: function(name, args, kwds) {
+        socket.emit('event', {
+          name: name,
+          args: args || [],
+          kwds: kwds || {}
+        });
+      },
+
+      request: function(method, args, kwds) {
+        var requestId = nextId++,
+            deferred = $q.defer();
+        socket.emit('request', {
+          id: requestId,
+          method: method,
+          args: args || [],
+          kwds: kwds || {}
+        });
+        pendingRequests[requestId] = deferred;
+        return deferred.promise;
+      },
+
+      on: function(name, f) {
+        if (callbacks[name] === undefined) {
+          socket.emit('subscribe', {
+            name: name
+          });
+          callbacks[name] = [];
+        }
+        callbacks[name].push(f);
+      }
+    };
+  };
+});

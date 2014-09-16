1ae7a0384e88f73f849bb0fb2e27d054f9a1a258
Kyle Raftogianis <kylerafto@gmail.com>
2014-09-15 20:00:23 -0700

    Emit 'exit' on exit

--- server/butler.js
+++ server/butler.js
@@ -1,3 +1,17 @@
+var process = require('process');
 var _ = require('underscore');
 
-module.exports = _.clone(require('./bus'));
+var butler = module.exports = _.clone(require('./bus'));
+
+process.on('exit', function(code) {
+  butler.emit('exit', code);
+});
+
+process.on('SIGINT', function() {
+  process.exit();
+});
+
+process.on('uncaughtException', function(err) {
+  console.log(err.stack);
+  process.exit(1);
+});

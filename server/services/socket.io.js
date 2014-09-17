57e1e11277fec88398bb6abfd09bf9ffccc47a99
Kyle Raftogianis <kylerafto@gmail.com>
2014-09-16 18:38:47 -0700

    Socket.io service

---
+++ server/services/socket.io.js
@@ -0,0 +1,52 @@
+var io = require('socket.io');
+var Q = require('q');
+var _ = require('underscore');
+
+var butler = require('../butler');
+
+function serve(port, hostname) {
+  var server = io(butler.call('server'), { serveClient: false });
+
+  server.on('connection', function(socket) {
+    socket.on('request', function(request) {
+      handle(request).done(function(response) {
+        socket.emit('response', response);
+      });
+    });
+  });
+
+  butler.on(function() {
+    // TODO: ensure objects are serializable first
+    server.emit('event', {
+      name: this.event,
+      params: _.toArray(arguments)
+    });
+  });
+}
+
+function handle(request) {
+  return Q['try'](function() {
+    var args = [request.method].concat(request.params)
+    return butler.call.apply(butler, args);
+  }).then(function(result) {
+    return {
+      result: result,
+      error: null,
+      id: request.id
+    };
+  }, function(err) {
+    return {
+      result: null,
+      error: err,
+      id: request.id
+    };
+  });
+}
+
+module.exports = {
+  depends: ['server'],
+
+  start: function() {
+    serve();
+  }
+};

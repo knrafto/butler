e44bae7635aa8d4ddcd53394db0a449ae5c0a017
Kyle Raftogianis <kylerafto@gmail.com>
2014-09-16 18:38:05 -0700

    HTTP server service

--- server/services/server.js
+++ server/services/server.js
@@ -1,50 +1,26 @@
-var Q = require('q');
+var http = require('http');
 var _ = require('underscore');
 
 var butler = require('../butler');
 
 function serve(port, hostname) {
-  var http = require('http').createServer();
-  var server = require('socket.io')(http, { serveClient: false });
-  http.listen(port, hostname);
-
-  server.on('connect', function(socket) {
-    socket.on('request', function(request) {
-      handle(request).done(function(response) {
-        socket.emit('response', response);
-      });
+  var server = new http.Server;
+  server.listen(port, hostname, function() {
+    butler.on('exit', function() {
+      server.close();
     });
   });
 
-  butler.on(function() {
-    server.emit('event', {
-      name: this.event,
-      params: _.toArray(arguments)
-    });
+  butler.register('server', function() {
+    return server;
   });
-}
 
-function handle(request) {
-  return Q['try'](function() {
-    return butler.call.apply(butler, [request.method].concat(request.params));
-  }).then(function(result) {
-    return {
-      result: result,
-      error: null,
-      id: request.id
-    };
-  }, function(err) {
-    return {
-      result: null,
-      error: err,
-      id: request.id
-    };
+  server.on('error', function() {
+    butler.emit('error', 'server', err);
   });
 }
 
 module.exports = {
-  name: 'server',
-
   start: function(config) {
     config = config || {};
     serve(config.port, config.hostname);
--- server/tests/server.spec.js
+++ server/tests/server.spec.js
@@ -1,162 +1,29 @@
-var EventEmitter = require('events').EventEmitter;
-var proxyquire = require('proxyquire');
 var Q = require('q');
 var _ = require('underscore');
 
+var butler = require('../butler')
+var service = require('../services/server');
+
 describe('server', function() {
-  var httpServer, server, socket, butler, http, io, service;
+  afterEach(function() {
+    butler.reset();
+  });
 
-  beforeEach(function() {
-    httpServer = { listen: jasmine.createSpy('listen') };
-
-    server = new EventEmitter;
-    socket = new EventEmitter;
-
-    butler = _.clone(require('../bus'));
-
-    http = {
-      createServer: jasmine.createSpy('createServer').andReturn(httpServer)
+  it('should start and close an HTTP server', function(done) {
+    var config = {
+      hostname: 'localhost',
+      port: 54010
     };
-
-    io = jasmine.createSpy().andReturn(server);
-
-    service = proxyquire('../services/server', {
-      '../butler': butler,
-      'http': http,
-      'socket.io': io
-    });
-
-    // for Q
-    spyOn(require('process'), 'nextTick').andCallFake(function(fn) { fn(); });
-  });
-
-  it('should be named "server"', function() {
-    expect(service.name).toBe('server');
-  });
-
-  it('should start an HTTP server', function() {
-    service.start({
-      hostname: 'www.example.com',
-      port: 9876
-    });
-
-    expect(http.createServer).toHaveBeenCalledWith();
-    expect(httpServer.listen).toHaveBeenCalledWith(9876, 'www.example.com');
-    expect(io).toHaveBeenCalledWith(httpServer, { serveClient: false });
-  });
-
-  it('should send events', function() {
-    var one = jasmine.createSpy('one');
-
-    server.on('event', one);
-
-    service.start();
-    server.emit('connect', socket);
-    butler.emit('foo', 1, 2)
-
-    expect(one).toHaveBeenCalledWith({
-      name: 'foo',
-      params: [1, 2]
-    });
-  });
-
-  it('should respond to requests', function() {
-    var one = jasmine.createSpy('one');
-
-    service.start();
-    server.emit('connect', socket);
-    socket.on('response', one);
-
-    butler.register('foo', function() {
-      return 'bar';
-    });
-
-    socket.emit('request', {
-      method: 'foo',
-      params: [1, 2],
-      id: 10
-    });
-
-    expect(one).toHaveBeenCalledWith({
-      result: 'bar',
-      error: null,
-      id: 10
-    });
-  });
-
-  it('should handle errors', function() {
-    var one = jasmine.createSpy('one');
-
-    service.start();
-    server.emit('connect', socket);
-    socket.on('response', one);
-
-    butler.register('foo', function() {
-      throw new Error('boom');
-    });
-
-    socket.emit('request', {
-      method: 'foo',
-      params: [1, 2],
-      id: 10
-    });
-
-    expect(one).toHaveBeenCalledWith({
-      result: null,
-      error: Error('boom'),
-      id: 10
-    });
-
-    socket.emit('request', {
-      method: 'bar',
-      params: [1, 2],
-      id: 10
-    });
-
-    expect(one).toHaveBeenCalledWith({
-      result: null,
-      error: Error('no delegate for method "bar"'),
-      id: 10
-    });
-  });
-
-  it('should resolve promises', function() {
-    var one = jasmine.createSpy('one');
-
-    service.start();
-    server.emit('connect', socket);
-    socket.on('response', one);
-
-    butler.register('foo', function() {
-      return Q('bar');
-    });
-
-    socket.emit('request', {
-      method: 'foo',
-      params: [1, 2],
-      id: 10
-    });
-
-    expect(one).toHaveBeenCalledWith({
-      result: 'bar',
-      error: null,
-      id: 10
-    });
-
-    butler.register('foo', function() {
-      return Q.reject(new Error('boom'));
-    });
-
-    socket.emit('request', {
-      method: 'foo',
-      params: [1, 2],
-      id: 10
-    });
-
-    expect(one).toHaveBeenCalledWith({
-      result: null,
-      error: Error('boom'),
-      id: 10
+    service.start(config);
+    var server = butler.call('server');
+    server.on('listening', function() {
+      expect(server.address()).toEqual({
+        port: 54010,
+        family: 'IPv4',
+        address: '127.0.0.1'
+      });
+      butler.emit('exit', 0);
+      server.on('close', done);
     });
   });
 });

1bb80e803aebf6706e527aca7b834f97bd77b917
Kyle Raftogianis <kylerafto@gmail.com>
2014-09-17 17:05:42 -0700

    Change config file name

--- server/run.js
+++ server/run.js
@@ -3,7 +3,7 @@ var process = require('process');
 
 var services = require('./services');
 
-var config_path = process.env['HOME'] + '/.config/butler/butler.cfg';
+var config_path = process.env['HOME'] + '/.config/butler/butler.conf';
 
 fs.readFile(config_path, 'utf8', function(err, data) {
   if (err) throw err;

af91c856bbc00b20742ba87a123ab7fe77fef17f
Kyle Raftogianis <knrafto@gmail.com>
2014-09-23 17:28:57 -0700

    Inline API key for now

--- mobile/www/js/lastfm.js
+++ mobile/www/js/lastfm.js
@@ -2,17 +2,14 @@ angular.module('lastfm', ['server', 'underscore'])
 
 .factory('lastfm', function($http, butler, _) {
   var url = 'http://ws.audioscrobbler.com/2.0/';
-  var getKey = butler.call('key.lastfm');
 
   function call(method, params) {
-    return getKey.then(function(apiKey) {
-      params = _.extend(params, {
-        api_key: apiKey,
-        format: 'json',
-        method: method
-      });
-      return $http.get(url, { params: params });
+    params = _.extend(params, {
+      api_key: 'xxx',
+      format: 'json',
+      method: method
     });
+    return $http.get(url, { params: params });
   }
 
   return {

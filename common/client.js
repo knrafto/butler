42409b447d8f192c1846c808204d87fc009bbb24
Kyle Raftogianis <knrafto@gmail.com>
2014-09-25 00:19:53 -0700

    Write JSON-RPC/event client

---
+++ common/client.js
@@ -0,0 +1,88 @@
+var EventEmitter = require('events').EventEmitter;
+var util = require('util');
+var WebSocket = require('ws');
+var _ = require('underscore');
+
+/**
+ * Create a Client that connects to a remote server over a
+ * websocket.
+ * @constructor
+ * @augments EventEmitter
+ * @property {Boolean} connected The WebSocket connection state.
+ */
+function Client() {
+  this.connected = false;
+  this.ws = null;
+
+  this.nextId = 0;
+  this.requests = {};
+}
+
+util.inherits(Client, EventEmitter);
+
+/**
+ * Open the connection on the given url and protocols, closing any previous
+ * connection. Upon a successful connection, the 'open' event will fire.
+ * @param {string} url The server URL.
+ * @param {(string|Array.<string>)=} protocols The protocols to use.
+ */
+Client.prototype.open = function(url, protocols) {
+  this.close();
+
+  this.forceClose = false;
+  var ws = this.ws = new WebSocket(url, protocols);
+  var self = this;
+
+  ws.onopen = function() {
+    self.connected = true;
+    self.emit('open');
+  };
+
+  ws.onclose = function(event) {
+    self.connected = false;
+    self.ws = null;
+    _.each(self.requests, function(callback) {
+      callback(new Error('WebSocket closed'));
+    });
+    self.emit('close', event.code, event.reason);
+  };
+
+  ws.onmessage = function(event) {
+    var message = JSON.parse(event.data);
+    if (message.event != null) {
+      self.emit('event', message.event, message);
+    } else {
+      var callback = self.requests[message.id];
+      if (callback) callback(message.error, message.result);
+    }
+  };
+};
+
+/**
+ * Close the connection, if it exists. This will cancel any pending requests
+ * and fire the 'close' event. The connection will not try to reconnect.
+ */
+Client.prototype.close = function() {
+  if (this.ws) this.ws.close();
+};
+
+/**
+ * Asynchronosly send a JSON-RPC request.
+ * @param {string} method The method name.
+ * @param {(Array|Object)} args The method arguments.
+ * @param {function(Error, *)} callback The asynchronous callback.
+ */
+Client.prototype.request = function(method, args, callback) {
+  if (!this.connected) throw new Error('WebSocket not connected');
+  var requestId = this.nextId++;
+  this.requests[requestId] = callback;
+  this.ws.send(JSON.stringify({
+    jsonrpc: '2.0',
+    id: requestId,
+    method: method,
+    params: args
+  }));
+};
+
+/** @module client */
+module.exports = Client;
---
+++ common/tests/client.spec.js
@@ -0,0 +1,204 @@
+var _ = require('underscore');
+require('ws');
+
+describe('Client', function() {
+  var WebSocket, socket, requests;
+  var client;
+
+  beforeEach(function() {
+    WebSocket = spyOn(require.cache[require.resolve('ws')], 'exports');
+    socket = {
+      open: function() {
+        socket.onopen();
+      },
+
+      close: function() {
+        socket.onclose({ code: 1006, reason: 'reason' });
+      },
+
+      send: function(data) {
+        requests.push(JSON.parse(data));
+      },
+
+      receive: function(data) {
+        socket.onmessage({
+          data: JSON.stringify(data)
+        });
+      }
+    };
+    requests = [];
+
+    var Client = require('../client');
+    client = new Client();
+
+    WebSocket.and.callFake(function() {
+      return socket;
+    });
+  });
+
+  describe('.open(url, [protocols])', function() {
+    it('should attempt to open a new connection', function() {
+      var url = 'ws://example.com';
+      var protocols = ['protocol1', 'protocol2'];
+      client.open(url, protocols);
+      expect(WebSocket).toHaveBeenCalledWith(url, protocols);
+    });
+
+    it('should emit "open" on success', function(done) {
+      client.open();
+      client.on('open', done);
+      socket.open();
+    });
+
+    it('should emit "close" on failure', function(done) {
+      client.open();
+      client.on('close', function(code, reason) {
+        expect(code).toEqual(1006);
+        expect(reason).toEqual('reason');
+        done();
+      });
+      socket.close();
+    });
+
+    it('should close any previous connection', function(done) {
+      client.open();
+      socket.close = done;
+      client.open();
+    });
+
+    it('should update "connected" property', function() {
+      expect(client.connected).toBe(false);
+      client.open();
+      expect(client.connected).toBe(false);
+      socket.open();
+      expect(client.connected).toBe(true);
+    });
+  });
+
+  describe('.close()', function() {
+    it('should close the connection', function(done) {
+      client.open();
+      socket.close = done;
+      client.close();
+    });
+
+    it('should emit "close"', function(done) {
+      client.open();
+      client.on('close', done);
+      client.close();
+    });
+
+    it('should do nothing when not open', function() {
+      client.on('close', function() {
+        expect(true).toBe(false);
+      });
+      client.close();
+    });
+
+    it('should update "connected" property', function() {
+      client.open();
+      socket.open();
+      expect(client.connected).toBe(true);
+      socket.close();
+      expect(client.connected).toBe(false);
+    });
+  });
+
+  describe('.request(method, args, callback)', function() {
+    it('should send numbered requests to the server', function() {
+      client.open();
+      socket.open();
+
+      client.request('foo', [1, 2], _.noop);
+      client.request('bar', {3: 4}, _.noop);
+      expect(requests).toEqual([
+        {
+          jsonrpc: '2.0',
+          id: 0,
+          method: 'foo',
+          params: [1, 2]
+        },
+        {
+          jsonrpc: '2.0',
+          id: 1,
+          method: 'bar',
+          params: {3: 4}
+        }
+      ]);
+    });
+
+    it('should throw when not open', function() {
+      client.open();
+      expect(function() {
+        client.request('foo', [1, 2], _.noop);
+      }).toThrow();
+    });
+
+    it('should call on success', function(done) {
+      client.open();
+      socket.open();
+
+      client.request('foo', [1, 2], function(err, result) {
+        expect(err).toBeFalsy();
+        expect(result).toBe('result');
+        done();
+      });
+      client.request('bar', {3: 4}, _.noop);
+
+      socket.receive({
+        id: 0,
+        error: null,
+        result: 'result'
+      });
+    });
+
+    it('should call on error', function(done) {
+      client.open();
+      socket.open();
+
+      client.request('foo', [1, 2], function(err, result) {
+        expect(err).toBe('oops');
+        expect(result).toBe(null);
+        done();
+      });
+      client.request('bar', {3: 4}, _.noop);
+
+      socket.receive({
+        id: 0,
+        error: 'oops',
+        result: null
+      });
+    });
+
+    it('should call on close', function(done) {
+      client.open();
+      socket.open();
+
+      client.request('foo', [1, 2], function(err, result) {
+        expect(err).toBeTruthy();
+        done();
+      });
+      client.request('bar', {3: 4}, _.noop);
+
+      socket.close();
+    });
+  });
+
+  it('should emit events', function(done) {
+    client.open();
+    socket.open();
+
+    var data = {
+      event: 'foo',
+      params: [1, 2]
+    };
+
+    client.on('event', function(name, event) {
+      expect(name).toEqual('foo');
+      expect(event).toEqual(data);
+      done();
+    });
+
+    socket.receive(data);
+  });
+});
